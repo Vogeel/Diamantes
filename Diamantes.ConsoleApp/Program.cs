0f570f4d8569ba862308241fbda84ec7d84a67d5
Vogeel <vitorvogel12@gmail.com>
2022-02-21 16:41:43 -0300

    Diamante com funções

--- Diamantes.ConsoleApp/Program.cs
+++ Diamantes.ConsoleApp/Program.cs
@@ -6,57 +6,106 @@ namespace Diamantes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            volta:
-            Console.WriteLine("Digite um numero (ímpar) para fazer o diamante, confomre maior o numero, mais sera o desenho");
-
-            int numero = int.Parse(Console.ReadLine());
-
+            int numero = 0;           
+        volta:
+            DigitarNumero(numero);
+          
             if (numero % 2 != 0)
             {
-                for (int i = 0; i < ((numero - 1) / 2); i++) // i = linha que vai ser atribuida
-                {
-                    for (int lugarlinha = 0; lugarlinha < numero; lugarlinha++) // lugar linha = lugar em que o x vai estar
-                    {
-                        if (lugarlinha >= ((numero - 1) / 2) - i && lugarlinha <= ((numero - 1) / 2) + i) // numero -1 / 2 = linha central, onde sempre vai ter x
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write("x");
-                            Console.ResetColor();
-                        }
-
-                        else
-                            Console.Write(" ");
-                    }
-                    Console.WriteLine();
-                }
-                for (int i = ((numero - 1) / 2); i < numero; i++)
-                {
-                    for (int lugarlinha = 0; lugarlinha < numero; lugarlinha++)
-                    {
-                        if (lugarlinha > ((numero - 1) / 2) - (numero - i) && lugarlinha < ((numero - 1) / 2) + (numero - i))
-                        {
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write("x");
-                            Console.ResetColor();
-                        }
-
-                        else
-                            Console.Write(" ");
-                    }
-                    Console.WriteLine();
-                }
-                Console.ReadKey();
+                ParteDeCimaELinhaAtribuida(numero);
+                ParteDeBaixoELinhaAtribuida(numero);
             }
-        
+
             else
             {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Erro, seu número é invalido para esta ocasião.");
-                Console.ResetColor();
+                MngErro ();
                 goto volta;
             }
             Console.ReadLine();
+
+            
         }
+
+        private static int DigitarNumero(int numero)
+        {
+            numero = 0;
+
+            Console.WriteLine("Digite um numero (ímpar) para fazer o diamante, confomre maior o numero, mais sera o desenho");
+
+             numero = int.Parse(Console.ReadLine());
+
+            return numero;
+        }
+      
+        static void LugarDeXnaLinhaBaixo(int numero, int i)
+        {
+            for (int lugarlinha = 0; lugarlinha < numero; lugarlinha++)
+            {
+                XcentralBiaxo(numero, i, lugarlinha);
+            }
+            Console.WriteLine();
+        }
+
+        static void ParteDeBaixoELinhaAtribuida(int numero)
+        {
+            for (int i = ((numero - 1) / 2); i < numero; i++)
+            {
+                LugarDeXnaLinhaBaixo(numero, i);
+            }
+            Console.ReadKey();
+        }
+
+        private static void XcentralBiaxo(int numero, int i, int lugarlinha)
+        {
+            if (lugarlinha > ((numero - 1) / 2) - (numero - i) && lugarlinha < ((numero - 1) / 2) + (numero - i))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("x");
+                Console.ResetColor();
+            }
+
+            else
+                Console.Write(" ");
+        }
+
+        private static void ParteDeCimaELinhaAtribuida(int numero)
+        {
+            for (int i = 0; i < ((numero - 1) / 2); i++) // i = linha que vai ser atribuida
+            {
+                LugarOndeXvaiEstarNaLinha(numero, i);
+            }
+        }
+
+        static void LugarOndeXvaiEstarNaLinha(int numero, int i)
+        {
+            for (int lugarlinha = 0; lugarlinha < numero; lugarlinha++) // lugar linha = lugar em que o x vai estar
+            {
+                Xcentral(numero, i, lugarlinha);
+            }
+            Console.WriteLine();
+        }
+
+        private static void Xcentral(int numero, int i, int lugarlinha)
+        {
+            if (lugarlinha >= ((numero - 1) / 2) - i && lugarlinha <= ((numero - 1) / 2) + i) // numero -1 / 2 = linha central, onde sempre vai ter x
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("x");
+                Console.ResetColor();
+            }
+
+            else
+                Console.Write(" ");
+        }
+
+        private static void MngErro()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Erro, seu número é invalido para esta ocasião.");
+            Console.ResetColor();
+        }
+
+        
     }
 }
